1476e98b6a904e259508579c250fd78d79dca1ec
harry <harrymac1972@gmail.com>
2024-02-20 16:44:08 -0700

    logic filled in for Tutor Session on Model Associations

--- models/index.js
+++ models/index.js
@@ -1,3 +1,12 @@
 
 
 // Associations will go here... Tutor session Tues to get this done on last one
+
+    // User has many Posts
+    // Post has one User
+
+    // Post has many Comments
+    // Comment has one Post
+
+    // User has many Comments [on other people's Posts]
+    // Comment has one User
