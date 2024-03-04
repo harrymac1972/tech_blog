da2352ad0b0f4668ed9a826085fddee987682d11
harry <harrymac1972@gmail.com>
2024-03-04 07:53:17 -0700

    tidy

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -6,7 +6,7 @@ const commentRoutes = require('./commentRoutes');
 
 
 router.use('/users', userRoutes);
-router.use('/projects', postRoutes);
+router.use('/posts', postRoutes);
 router.use('/comments', commentRoutes);
 
 
