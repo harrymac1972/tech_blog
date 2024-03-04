8ca3a4853fa9e37ae40b3e5af08c3b7ff1be809c
harry <harrymac1972@gmail.com>
2024-03-04 06:45:54 -0700

    homeRoutes.js login route added

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -50,4 +50,13 @@ router.get("/post/:id", async (req, res) => {
   }
 });
 
+router.get('/login', (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect('/dashboard');
+    return;
+  }
+  res.render('login');
+});
+
+
 module.exports = router;
