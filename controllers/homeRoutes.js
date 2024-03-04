4e82cf33ac6824d1ca627be42e40f31daeb91581
harry <harrymac1972@gmail.com>
2024-03-04 05:59:23 -0700

    homeRoutes.js import includes Comment

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,6 +1,6 @@
 
 const router = require('express').Router();
-const { Post, User } = require('../models');
+const { User, Post, Comment } = require('../models');
 
 
 router.get('/', async (req, res) => {
