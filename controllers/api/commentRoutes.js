81ec50ee9072e91d76edab6a1adaf18e08b06333
harry <harrymac1972@gmail.com>
2024-03-04 23:56:41 -0700

    commentRoutes.js created finished

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -4,3 +4,21 @@ const { Comment } = require('../../models');
 const isAuth = require('../../utils/isAuth');
 
 
+// Create comment
+router.post('/', isAuth, async (req, res) => {
+    try {
+      const newComment = await Comment.create({
+        comment_text: req.body.comment_text,
+        user_id: req.session.user_id,
+        post_id: req.body.post_id
+      });
+  
+      res.status(201).json(newComment);
+    } catch (err) {
+      console.error(err);
+      res.status(400).json({ error: 'No comment created' });
+    }
+  });
+  
+  
+  module.exports = router;
