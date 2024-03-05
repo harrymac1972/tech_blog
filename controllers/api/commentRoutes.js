31ff11df7583b11707a3a93537678aaa9c94ed0e
harry <harrymac1972@gmail.com>
2024-03-05 00:06:32 -0700

    commentRoutes.js Delete added

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -11,14 +11,30 @@ router.post('/', isAuth, async (req, res) => {
         comment_text: req.body.comment_text,
         user_id: req.session.user_id,
         post_id: req.body.post_id
-      });
-  
+      });  
       res.status(201).json(newComment);
     } catch (err) {
       console.error(err);
       res.status(400).json({ error: 'No comment created' });
     }
   });
+
+  // Delete comment
+router.delete('/:id', isAuth, async (req, res) => {
+    const comment_id = parseInt(req.params.id);  
+    try {
+      const delComment = await Comment.findByPk(comment_id);  
+      if (!delComment) {
+        res.status(404).json({ error: 'No Comment found' });
+        return;
+      }  
+      await delComment.destroy();  
+      res.status(200).json({ message: 'Comment deleted' });
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Server error' });
+    }
+  });
   
-  
+
   module.exports = router;
