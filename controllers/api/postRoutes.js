7d5b89af51f04d46522562902cf8c87c2d6d56fd
harry <harrymac1972@gmail.com>
2024-03-04 23:18:13 -0700

    postRoutes.js Delete added

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -42,5 +42,26 @@ router.put('/:id', isAuth, async (req, res) => {
     }
   });
 
+  // Delete
+  router.delete('/:id', isAuth, async (req, res) => {
+    try {
+      const deletedRecords = await Post.destroy({
+        where: {
+          id: req.params.id,
+          user_id: req.session.user_id,
+        },
+      });
+  
+      if (deletedRecords === 0) {
+        res.status(404).json({ message: 'No post found' });
+        return;
+      }
+  
+      res.status(200).json({ message: 'Post deleted' });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
 
 module.exports = router;
