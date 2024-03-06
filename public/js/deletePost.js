6f251a6228bf1d91cba0cfec87ea873d204fc5f5
harry <harrymac1972@gmail.com>
2024-03-05 22:36:35 -0700

    deletePost.js added

---
+++ public/js/deletePost.js
@@ -0,0 +1,23 @@
+
+    // Delete Post
+const handlePostDeletion = async (event) => {
+    event.preventDefault();
+    const postId = event.target.dataset.postId;  
+    try {
+      const response = await fetch(`/api/posts/${postId}`, {
+        method: 'DELETE',
+      });
+      if (response.ok) {
+        window.location.replace('/dashboard');
+      } else {
+        alert(response.statusText);
+      }
+    } catch (error) {
+      alert('ERROR. Please try again.');
+    }
+  };
+  
+  // listen
+  document.querySelectorAll('.delete-post-button').forEach((button) => {
+    button.addEventListener('click', handlePostDeletion);
+  });
