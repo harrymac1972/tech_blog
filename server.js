120918fc9f1275f0268aeefc6e8444b2e2f09670
harry <harrymac1972@gmail.com>
2024-03-06 01:10:11 -0700

    server.js repaired

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 
 const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const path = require('path');
+const helpers = require('./utils/helpers');
+require('dotenv').config();
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
